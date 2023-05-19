4ed428c51ec6ff801103b97a1fe16e826235d846
MarcusFabI0 <fabio63399@gmail.com>
2023-05-18 23:18:39 -0300

    improving footer and navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  user: any;
 
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    const userJSON = this.authService.userAuth();
+    if (userJSON) {
+      this.user = JSON.parse(userJSON);
+    } else {
+      console.log('Usuário não encontrado.');
+    }
+  }
+
+  logout() {
+    this.authService.clearStorage();
+    this.router.navigateByUrl('/login');
+  }
 }
